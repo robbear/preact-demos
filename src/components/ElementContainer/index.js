963fffb1562fcf2c30e72996523835bc508948db
Rob Bearman <robbear@hyperfine.com>
2017-07-06 19:10:25 +0000

    Add Elix ListBox

--- src/components/ElementContainer/index.js
+++ src/components/ElementContainer/index.js
@@ -3,20 +3,11 @@ import style from './style';
 
 export default class ElementContainer extends Component {
 
-  constructor(props) {
-    super(props);
-    console.dir(props);
-    this.state = {
-      title: props.title,
-      description: props.description
-    };
-  }
-
 	render() {
 		return (
 			<div class={style.element_container}>
-				<h1>{this.state.title}</h1>
-				<h3>{this.state.description}</h3>
+				<h1>{this.props.title}</h1>
+				<h3>{this.props.description}</h3>
 				{this.props.children}
 			</div>
 		);
--- src/routes/listboxdemo/index.js
+++ src/routes/listboxdemo/index.js
@@ -1,12 +1,45 @@
 import { h, Component } from 'preact';
 import ElementContainer from '../../components/ElementContainer'
+import ListBox from '@elix/elix/elements/ListBox';
+import items from './items';
 import './style.css';
 
 export default class ListBoxDemo extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: items,
+      selectedIndex: 0
+    };
+  }
+
+  componentDidMount() {
+    this.listBoxElem.addEventListener('selected-index-changed', event => {
+      this.setState({
+        selectedIndex: event.detail.selectedIndex
+      });
+    });
+  }
+  
 	render() {
+    const itemElements = this.state.items && this.state.items.map(item => (
+      <div key={item}>{item}</div>
+    ));
+    const description = `Selection: ${this.state.items[this.state.selectedIndex]}`;
+
 		return (
-		  <ElementContainer title="ListBox" description="ListBox description text">
-		    <div>Div text is placed temporarily here</div>
+		  <ElementContainer 
+		    title="React app with an Elix list box" 
+		    description={description}>
+        <elix-list-box
+            ref={(el) => { this.listBoxElem = el; }}
+            id="list"
+            aria-label="Fruits"
+            selected-index={this.state.selectedIndex}
+            style={{height: "295px", maxWidth: "400px"}}>
+          {itemElements}
+        </elix-list-box>
 		  </ElementContainer>
 		);
 	}
